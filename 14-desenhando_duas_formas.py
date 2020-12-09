fe2cc688b78a106e2d8be0a7f27128eb23a24150
vagner-infnet <75703293+vagner-infnet@users.noreply.github.com>
2020-12-09 08:02:22 -0300

    Dois desenhos num mesmo painel

--- 14-desenhando_duas_formas.py
+++ 14-desenhando_duas_formas.py
@@ -10,9 +10,9 @@ def exibe_janela_e_desenha_2formas():
     
     largura_da_JANELA = 400
     altura_da_JANELA = 400
-    tupla_largura_altura = (largura_da_JANELA, altura_da_JANELA)
+    largura_altura_da_JANELA = (largura_da_JANELA, altura_da_JANELA)
     #JANELA > PRECISAMOS GRAVAR UMA REFERÊNCIA DA JANELA DO PROGRAMA
-    JANELA = pygame.display.set_mode(tupla_largura_altura)
+    JANELA = pygame.display.set_mode(largura_altura_da_JANELA)
     pygame.display.set_caption("Desenhando duas formas")
     pygame.display.init()
     ####################################################
@@ -20,14 +20,13 @@ def exibe_janela_e_desenha_2formas():
     altura_do_PAINEL_1 = altura_da_JANELA/2       
     largura_altura_do_PAINEL_1 = (largura_do_PAINEL_1, altura_do_PAINEL_1)
     PAINEL_1 = pygame.surface.Surface(largura_altura_do_PAINEL_1)
-
-    y_inicial_do_PAINEL_1 = 0
-    x_inicial_do_PAINEL_1 = 0
+    ####################################################
+    # não houve necessidade de resetar a janela
+    ####################################################
     def reseta_painel_1():
         print("desenhando painel_1")
         cor_do_PAINEL_1 = (255, 255, 255)  # branco
         PAINEL_1.fill(cor_do_PAINEL_1)  # pintando o painel com a cor desejada
-    reseta_painel_1()
     ####################################################
     #configurando superfície
     def forma_1(contador):
@@ -36,9 +35,11 @@ def exibe_janela_e_desenha_2formas():
         y_inicial_da_FORMA_1 = 0
         largura_da_FORMA_1 = contador        
         altura_da_FORMA_1 = contador  
+        # evitando que o desenho superior cresça e cubra o desenho inferior
         if altura_da_FORMA_1 > altura_do_PAINEL_1/2:  
             altura_da_FORMA_1 = altura_do_PAINEL_1/2    
         cor_da_FORMA_1 = (0, 0, 255)  # azul
+        # desenhando no painel
         pygame.draw.rect(PAINEL_1, cor_da_FORMA_1, (x_inicial_da_FORMA_1, y_inicial_da_FORMA_1, largura_da_FORMA_1, altura_da_FORMA_1))
     def forma_2(contador):
         print("desenhando forma_2")
@@ -47,6 +48,7 @@ def exibe_janela_e_desenha_2formas():
         largura_da_FORMA_2 = contador
         altura_da_FORMA_2 = contador       
         cor_da_FORMA_2 = (255, 0, 0)  # vermelho
+        # desenhando no painel
         pygame.draw.rect(PAINEL_1, cor_da_FORMA_2, (x_inicial_da_FORMA_2, y_inicial_da_FORMA_2, largura_da_FORMA_2, altura_da_FORMA_2))
         ####################################################
     continuar_no_loop_while = True
@@ -62,14 +64,20 @@ def exibe_janela_e_desenha_2formas():
                 continuar_no_loop_while = False
                 #continue  # sair deste loop for
         contador = contador + 1
-        #painel_1(contador)
+        # apagando o conteúdo do painel
         reseta_painel_1()
+        # desenhando a forma 1, com novo valor
         forma_1(contador)
+        # desenhando a forma 2, com novo valor
         forma_2(contador)
+        # inserindo o painel, que teve as formas redesenhadas, na janela
+        y_inicial_do_PAINEL_1 = 0
+        x_inicial_do_PAINEL_1 = 0
+        JANELA.blit(PAINEL_1, (x_inicial_do_PAINEL_1, y_inicial_do_PAINEL_1))  
         # Fazendo a janela exibir os componentes com seus valores atualizados
-        JANELA.blit(PAINEL_1, (x_inicial_do_PAINEL_1, y_inicial_do_PAINEL_1))  # inserindo o painel na janela
         pygame.display.update()
-        pygame.time.Clock().tick(60) # nº de atualizações por segundo
+        # nº de atualizações e loops while por segundo
+        pygame.time.Clock().tick(60) 
     ####################################################
     # após ter feito tudo que queríamos, fechamos o programa:
     #pygame.display.quit() 
