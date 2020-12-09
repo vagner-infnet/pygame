37eebfb26a9a5bd6f74fdf9acd04120e73aba666
vagner-infnet <75703293+vagner-infnet@users.noreply.github.com>
2020-12-09 07:54:15 -0300

    Desenhando dentro do painel

--- 13-desenhando_uma_forma.py
+++ 13-desenhando_uma_forma.py
@@ -10,9 +10,9 @@ def exibe_janela_e_desenha_1forma():
     
     largura_da_JANELA = 400
     altura_da_JANELA = 400
-    tupla_largura_altura = (largura_da_JANELA, altura_da_JANELA)
+    largura_altura_da_JANELA = (largura_da_JANELA, altura_da_JANELA)
     #JANELA > PRECISAMOS GRAVAR UMA REFERÊNCIA DA JANELA DO PROGRAMA
-    JANELA = pygame.display.set_mode(tupla_largura_altura)
+    JANELA = pygame.display.set_mode(largura_altura_da_JANELA)
     pygame.display.set_caption("Desenhando uma forma")
     pygame.display.init()
     ####################################################
@@ -21,13 +21,11 @@ def exibe_janela_e_desenha_1forma():
     largura_altura_do_PAINEL_1 = (largura_do_PAINEL_1, altura_do_PAINEL_1)
     PAINEL_1 = pygame.surface.Surface(largura_altura_do_PAINEL_1)
 
-    y_inicial_do_PAINEL_1 = 0
-    x_inicial_do_PAINEL_1 = 0
     def reseta_painel_1():
         print("desenhando painel_1")
         cor_do_PAINEL_1 = (255, 255, 255)  # branco
         PAINEL_1.fill(cor_do_PAINEL_1)  # pintando o painel com a cor desejada
-    #JANELA.blit(PAINEL_1, (x_inicial_do_PAINEL_1, y_inicial_do_PAINEL_1))  # inserindo o painel na janela
+    #
     reseta_painel_1()
     ####################################################
     #configurando superfície
@@ -40,8 +38,7 @@ def exibe_janela_e_desenha_1forma():
         largura_da_FORMA_1 = contador        
         altura_da_FORMA_1 = contador 
         pygame.draw.rect(PAINEL_1, cor_da_FORMA_1, (x_inicial_da_FORMA_1, y_inicial_da_FORMA_1, largura_da_FORMA_1, altura_da_FORMA_1))
-        #AGORA QUE DESENHEI UM RECT NO PAINEL, DEVO PEDIR À JANELA PARA RECEBER O PAINEL
-        #JANELA.blit(PAINEL_1, (x_inicial_do_PAINEL_1, y_inicial_do_PAINEL_1))  # inserindo o painel na janela
+        #
     ####################################################
     continuar_no_loop_while = True
     contador = 0
@@ -58,13 +55,19 @@ def exibe_janela_e_desenha_1forma():
         contador = contador + 1
         if contador == altura_do_PAINEL_1:
             contador = 0
-        #painel_1(contador)
+        # apagando o conteúdo do painel
         reseta_painel_1()
+        # o desenho que estava no painel já foi apagado
+        # desenhando novamente, com um novo valor do contador
         forma_1(contador)
+        # inserindo o painel na janela
+        x_inicial_do_PAINEL_1 = 0
+        y_inicial_do_PAINEL_1 = 0
+        JANELA.blit(PAINEL_1, (x_inicial_do_PAINEL_1, y_inicial_do_PAINEL_1))  
         # Fazendo a janela exibir os componentes com seus valores atualizados
-        JANELA.blit(PAINEL_1, (x_inicial_do_PAINEL_1, y_inicial_do_PAINEL_1))  # inserindo o painel na janela
         pygame.display.update()
-        pygame.time.Clock().tick(60) # nº de atualizações por segundo
+        # nº de loops e atualizações por segundo
+        pygame.time.Clock().tick(60) 
     ####################################################
     # após ter feito tudo que queríamos, fechamos o programa:
     #pygame.display.quit() 
