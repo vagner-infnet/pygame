ba082ecc16ed18c551eacaf549050452a34364ea
vagner-infnet <75703293+vagner-infnet@users.noreply.github.com>
2020-12-08 21:29:37 -0300

    ...

--- 07-exibe_janela_e_escuta_click_em_varios_botoes.py
+++ 07-exibe_janela_e_escuta_click_em_varios_botoes.py
@@ -6,15 +6,13 @@ a tela fecha automatiamente.
 """
 import pygame
 
-def exibe_janela_e_escuta_click_no_botao_sair_e_outros_botoes():
+def exibe_janela_e_escuta_click_em_varios_botoes():
     
     largura_da_JANELA = 400
     altura_da_JANELA = 400
     tupla_largura_altura = (largura_da_JANELA, altura_da_JANELA)
-
-    # display significa tela. Precisamos configurar o modo de exibição da tela > suas dimensões > sua altura e largura
     pygame.display.set_mode(tupla_largura_altura)
-    # init significa iniciar > iniciar o programa, exibindo sua janela
+    pygame.display.set_caption("Pressione o botão sair para encerrar.")
     pygame.display.init()
 
     print(f"pygame.KEYDOWN: {pygame.KEYDOWN}")  # KEYDOWN == TECLA PRESSIONADA > 768
@@ -32,16 +30,16 @@ def exibe_janela_e_escuta_click_no_botao_sair_e_outros_botoes():
 
             if event.type == pygame.QUIT:
                 continuar_no_loop_while = False
-                #continue  # sair deste loop for
+                continue  # abandono o loop while
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    print("seta para direita pressionada")       
+                    pygame.display.set_caption("seta para direita pressionada")       
                 if event.key == pygame.K_LEFT:
-                    print("seta para esquerda pressionada")   
+                    pygame.display.set_caption("seta para esquerda pressionada")   
                 if event.key == pygame.K_a:
-                    print("letra A pressionada")        
+                    pygame.display.set_caption("letra A pressionada")        
     ####################################################
     # após ter feito tudo que queríamos, fechamos o programa:
     #pygame.display.quit() 
 
-exibe_janela_e_escuta_click_no_botao_sair_e_outros_botoes()
+exibe_janela_e_escuta_click_em_varios_botoes()
