ab21783afa2ee061264135ab2853c27bb727be52
vagner-infnet <75703293+vagner-infnet@users.noreply.github.com>
2020-12-08 21:02:54 -0300

    display-set_mode()-init()

--- 01-tela vazia.py
+++ 01-tela vazia.py
@@ -8,17 +8,23 @@ import pygame
 
 def exibe_janela_vazia_e_faz_nada():
 
+    #configurando as dimensões da janela
     largura_da_JANELA = 400
     altura_da_JANELA = 400
     tupla_largura_altura = (largura_da_JANELA, altura_da_JANELA)
 
-    # display significa tela. Precisamos configurar o modo de exibição da tela > suas dimensões > sua altura e largura
+    # display significa tela/janela. 
+    # set_mode() recebe as dimensões em forma de tupla
     pygame.display.set_mode(tupla_largura_altura)
     
-    # init significa iniciar > iniciar o programa, exibindo sua janela
+    # init significa iniciar > 
+    # iniciamos o programa, exibindo sua janela
     pygame.display.init()
-    ... # não fazemos nada
-    # quit significa sair > sair do programa que está com a janela aberta
+
+    ... # por enquanto não fazemos nada
+
+    # quit significa sair > 
+    # sairemos automaticamente do programa
     #pygame.display.quit()
 
 exibe_janela_vazia_e_faz_nada()
