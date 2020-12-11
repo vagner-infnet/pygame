3b8b15f75f7862ee0fb9fbb8f294dd6dd3fedcd9
vagner-infnet <75703293+vagner-infnet@users.noreply.github.com>
2020-12-10 21:05:57 -0300

    ...

--- 16-inserindo_texto-com teclas.py
+++ 16-inserindo_texto-com teclas.py
@@ -22,11 +22,11 @@ def exibe_janela_e_desenha_2formas_com_teclas():
     ####################################################
     font_family_1 = 'Comic Sans MS'
     font_size_1 = 30
-    myfont_1 = pygame.font.SysFont(font_family_1, font_size_1) 
+    MyFont_1 = pygame.font.SysFont(font_family_1, font_size_1) 
     ####################################################
     font_family_2 = 'None'
     font_size_2 = 15
-    myfont_2 = pygame.font.SysFont(font_family_2, font_size_2)      
+    MyFont_2 = pygame.font.SysFont(font_family_2, font_size_2)      
     ####################################################
     largura_do_PAINEL_1 = largura_da_JANELA/2
     altura_do_PAINEL_1 = altura_da_JANELA/2       
@@ -39,24 +39,24 @@ def exibe_janela_e_desenha_2formas_com_teclas():
         PAINEL_1.fill(cor_do_PAINEL_1)  # pintando o painel com a cor desejada
     ####################################################
     #configurando superfície
-    def forma_1(contador):
+    def texto_1(contador):
         print("escrevendo texto_1")
         distancia_da_esq_para_a_dir_do_TEXTO_1 = 0
         distancia_de_cima_para_baixo_do_TEXTO_1 = 0
         cor_do_TEXTO_1 = (0, 0, 255)  # azul
         #gerando uma imagem com o texto
-        text = myfont_1.render("TEXTO_1", True, cor_do_TEXTO_1)
+        text_img = MyFont_1.render("TEXTO_1", True, cor_do_TEXTO_1)
         #inserindo o texto-imagem no painel
-        PAINEL_1.blit(text, (distancia_da_esq_para_a_dir_do_TEXTO_1, distancia_de_cima_para_baixo_do_TEXTO_1))
-    def forma_2(contador):
+        PAINEL_1.blit(text_img, (distancia_da_esq_para_a_dir_do_TEXTO_1, distancia_de_cima_para_baixo_do_TEXTO_1))
+    def texto_2(contador):
         print("escrevendo texto_2")
         distancia_da_esq_para_a_dir_do_TEXTO_2 = 0
         distancia_de_cima_para_baixo_do_TEXTO_2 = altura_do_PAINEL_1/2
         cor_do_TEXTO_2 = (255, 0, 0)  # vermelho
         #gerando uma imagem com o texto
-        text = myfont_2.render("TEXTO_2", True, cor_do_TEXTO_2)
+        text_img = MyFont_2.render("TEXTO_2", True, cor_do_TEXTO_2)
         #inserindo o texto-imagem no painel
-        PAINEL_1.blit(text, (distancia_da_esq_para_a_dir_do_TEXTO_2, distancia_de_cima_para_baixo_do_TEXTO_2))
+        PAINEL_1.blit(text_img, (distancia_da_esq_para_a_dir_do_TEXTO_2, distancia_de_cima_para_baixo_do_TEXTO_2))
         ####################################################
     continuar_no_loop_while = True
     contador = 0
@@ -75,12 +75,12 @@ def exibe_janela_e_desenha_2formas_com_teclas():
                     print("seta para direita pressionada")   
                     contador = contador + 1  
                     #reseta_painel_1()
-                    forma_1(contador)
+                    texto_1(contador)
                 if event.key == pygame.K_LEFT:
                     print("seta para esquerda pressionada")  
                     contador = contador + 1
                     #reseta_painel_1()
-                    forma_2(contador)
+                    texto_2(contador)
                 if event.key == pygame.K_SPACE:
                     print("tecla espaço pressionada") 
                     contador = 0
@@ -88,8 +88,8 @@ def exibe_janela_e_desenha_2formas_com_teclas():
         contador = contador + 1
         #painel_1(contador)
         #reseta_painel_1()
-        #forma_1(contador)
-        #forma_2(contador)
+        #texto_1(contador)
+        #texto_2(contador)
         # Fazendo a janela exibir os componentes com seus valores atualizados
         distancia_da_esq_para_a_dir_do_PAINEL_1 = 0
         distancia_de_cima_para_baixo_do_PAINEL_1 = 0
